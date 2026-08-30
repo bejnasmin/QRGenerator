a81a8a804f55d9b030970533c28343a04e4c560f
Bejna Asmin Orcen <orcenbejnaasmin@gmail.com>
2026-08-30 21:23:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ url = input("enter url to generate qr code: ")
 
 qr_code = pyqrcode.create(url)
 qr_code.svg('qrcode.svg',scale = 5) #scale burda boyut Yani burada create() QR kodu oluşturuyor, svg() ise oluşturulan QR kodu bir görsel dosyasına kaydediyor.
+
